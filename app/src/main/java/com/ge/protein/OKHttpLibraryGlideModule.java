c7ef32b370983b24de0542f5ff72729de3baad1b
gejiaheng <gejiaheng@gmail.com>
2017-09-13 18:55:14 +0800

    Glide uses OkHttp

---
+++ app/src/main/java/com/ge/protein/OKHttpLibraryGlideModule.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 Jiaheng Ge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ge.protein;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.LibraryGlideModule;
+
+import java.io.InputStream;
+
+@GlideModule
+public class OKHttpLibraryGlideModule extends LibraryGlideModule {
+
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory());
+    }
+}
